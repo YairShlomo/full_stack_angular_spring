44843cf7e5b9c146801d9da4b87b774bb73add0a
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-01-22 18:07:12 +0200

    menu,footer components, service
    authentication logic-hardcoded authentication
    logout

--- frontend/todo/src/app/app.module.ts
+++ frontend/todo/src/app/app.module.ts
@@ -7,14 +7,20 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { ErrorComponent } from './error/error.component';
-import { ListTodosComponent } from './list-todos/list-todos.component'
+import { ListTodosComponent } from './list-todos/list-todos.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogoutComponent } from './logout/logout.component'
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     LoginComponent,
     ErrorComponent,
-    ListTodosComponent
+    ListTodosComponent,
+    MenuComponent,
+    FooterComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -13,19 +14,20 @@ export class LoginComponent implements OnInit {
   invalidLogIn = false
 
   //Router 
-  constructor(private router : Router) { }
+  constructor(private router : Router,
+    private hardcodedAuthenticationService:HardcodedAuthenticationService) { }
 
   ngOnInit() {
   }
   
   handleLogin() {
-    if(this.username==="Yair144" && this.password==="dummy") {
+    if(this.hardcodedAuthenticationService.authenticate(this.username,this.password)) {
       //Redirect to Welcome Page
       this.router.navigate(['welcome',this.username])
       this.invalidLogIn = false 
     } else {
       this.invalidLogIn = true
     }
-
   }
+  
 }
